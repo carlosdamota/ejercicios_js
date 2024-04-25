c34fac324f466cc9fecae728c91506d296007a34
Carlos Damota <cdamota@msn.com>
2024-04-25 09:41:55 +0200

    0.0.1

---
+++ ejercicios_js.js
@@ -0,0 +1,12 @@
+
+const value = document.getElementById("campo").value;
+console.log(value)
+
+let intentos = [1, 5, 10, 12]
+console.log(intentos[4])
+
+let siglas = ['FMI', 'BID', ['ONU', 'UNESCO'], 'BIRF']
+console.log(siglas[2][1]);
+
+let paises = [['MX', 'CA'], 'ES']
+console.log(paises[0][0][0])
